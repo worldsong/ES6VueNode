b207279ce8a944c499a293bb125c0d63dd9916fa
worldsong <772083612@qq.com>
2018-12-05 22:00:48 +0800

    ES6学习环境搭建。

---
+++ lesson1_ES6语法/es5.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var resusts = [1, 2, 3].map(function (x) {
+  return x + 2;
+});
---
+++ lesson1_ES6语法/es6.js
@@ -0,0 +1,4 @@
+let resusts = [1, 2, 3].map(x => x + 2);
+console.log('song')
+console.log(resusts);
+document.body.innerText= resusts;
