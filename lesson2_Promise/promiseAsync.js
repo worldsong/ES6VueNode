029b533a64fef35e8ab539639d2c570f4620ad5a
worldsong <772083612@qq.com>
2018-12-09 21:14:47 +0800

    Promise方式的异步解决方案。

---
+++ lesson2_Promise/promiseAsync.js
@@ -0,0 +1,30 @@
+// 煮开水
+let boilWater = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('boilWater');
+        resolve('boilWater')
+    }
+    , 10 * 1000));  // 10秒钟
+
+// 洗杯子
+let washGlass = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('washGlass');
+        resolve('washGlass')
+    }
+    , 2 * 1000));  // 2秒钟
+
+// 准备茶叶
+let prepareTeaLeaves = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('prepareTeaLeaves');
+        resolve('prepareTeaLeaves')
+    }
+    , 1 * 1000));  // 1秒钟
+
+console.time('promiseAsync')
+Promise.all([
+    boilWater(),
+    washGlass(),
+    prepareTeaLeaves()
+]).then(()=> console.timeEnd('promiseAsync'));
