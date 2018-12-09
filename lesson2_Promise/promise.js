06a9de73b9e4b261d54372d3d2d29efd16156eea
worldsong <772083612@qq.com>
2018-12-09 21:01:30 +0800

    Promise方式的同步解决方案的优化写法。

--- lesson2_Promise/promise.js
+++ lesson2_Promise/promise.js
@@ -1,40 +1,29 @@
 // 煮开水
-var boilWater = function(){
-   return new Promise(function(resolve){
-       setTimeout(function(){
-           console.log('boilWater')
-           resolve('boilWater')
-       }, 10 * 1000); // 10秒钟
-   })
-}
+let boilWater = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('boilWater');
+        resolve('boilWater')
+    }
+    , 10 * 1000));  // 10秒钟
 
 // 洗杯子
-var washGlass = function(){
-    return new Promise(function(resolve){
-        setTimeout(function(){
-            console.log('washGlass')
-            resolve('washGlass')
-        }, 2 * 1000); // 2秒钟
-    })
-}
+let washGlass = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('washGlass');
+        resolve('washGlass')
+    }
+    , 2 * 1000));  // 2秒钟
 
 // 准备茶叶
-var prepareTeaLeaves = function(){
-    return new Promise(function(resolve){
-        setTimeout(function () {
-            console.log('prepareTeaLeaves')
-            resolve('prepareTeaLeaves')
-        }, 1 * 1000); // 1秒钟
-    })
-}
+let prepareTeaLeaves = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('prepareTeaLeaves');
+        resolve('prepareTeaLeaves')
+    }
+    , 1 * 1000));  // 1秒钟
+
 console.time('promise')
 boilWater()
-.then(function () {
-    return washGlass()
-})
-.then(function () {
-    return prepareTeaLeaves()
-})
-.then(function () {
-    console.timeEnd('promise');
-})
+.then(()=> washGlass())
+.then(()=> prepareTeaLeaves())
+.then(()=> console.timeEnd('promise'))
