c0d7e0d66cdd8b6bf51bbd31d6685f9525df1cdf
worldsong <772083612@qq.com>
2018-12-09 21:22:24 +0800

    async/await方式的同步解决方案的一般写法。

---
+++ lesson2_Promise/async.js
@@ -0,0 +1,32 @@
+// 煮开水
+let boilWater = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('boilWater');
+        resolve('boilWater')
+    }
+    , 10 * 1000));  // 10秒钟
+
+// 洗杯子
+let washGlass = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('washGlass');
+        resolve('washGlass')
+    }
+    , 2 * 1000));  // 2秒钟
+
+// 准备茶叶
+let prepareTeaLeaves = () => new Promise(resolve => setTimeout(
+    () => {
+        console.log('prepareTeaLeaves');
+        resolve('prepareTeaLeaves')
+    }
+    , 1 * 1000));  // 1秒钟
+
+console.time('async')
+let makeTea = async function () {
+    var boiledWater = await boilWater();
+    var washedGlass = await washGlass();
+    var teaLeaves = await prepareTeaLeaves();
+    console.timeEnd('async');
+}
+makeTea();
