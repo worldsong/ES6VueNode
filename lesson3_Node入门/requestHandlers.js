42b81d7f83828c0983b2316419ec531e1503106d
worldsong <772083612@qq.com>
2018-12-12 22:04:02 +0800

    模拟阻塞操作。

--- lesson3_Node入门/requestHandlers.js
+++ lesson3_Node入门/requestHandlers.js
@@ -1,5 +1,10 @@
 function start() {
     console.log("Request handler 'start' was called.");
+    function sleep(milliSeconds){
+        var startTime = new Date().getTime();
+        while(new Date().getTime() < startTime + milliSeconds);
+    }
+    sleep(15000);
     return "Hello start."
 }
 
