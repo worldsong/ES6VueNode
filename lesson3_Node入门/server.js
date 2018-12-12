e039d8f8f7817f5c1579f025e0e886b972cdfc5e
worldsong <772083612@qq.com>
2018-12-12 21:19:11 +0800

    找出浏览器请求的URL路径，区分不同的请求。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var url = require("url");
 
 function start() {
     function onRequest(request, response) { // 使用request和response对象处理HTTP请求的细节，并响应请求
-        console.log('Request received');
+        var pathname = url.parse(request.url).pathname;
+        console.log('Request for ' + pathname + ' received');
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello world"); // 响应请求，回复文本消息
         response.end(); // 结束响应
