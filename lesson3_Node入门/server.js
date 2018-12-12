e26bf42994e69b20e618654a6f3e8c45de2ff74f
worldsong <772083612@qq.com>
2018-12-12 20:52:36 +0800

    服务器是如何处理请求的，分析回调函数onRequest。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 
-function onRequest(request, response) {
+function onRequest(request, response) { // 使用request和response对象处理HTTP请求的细节，并响应请求
     console.log('Request received');
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
+    response.write("Hello world"); // 响应请求，回复文本消息
+    response.end(); // 结束响应
 }
 
 http.createServer(onRequest).listen(8888);
