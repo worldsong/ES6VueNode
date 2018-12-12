0b870471bfa32ba450d0b07f24cd689f1f0225e5
worldsong <772083612@qq.com>
2018-12-12 21:47:56 +0800

    在start()添加了handle参数，并且把handle对象作为第一个参数传递给了route()回调函数。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -1,12 +1,12 @@
 var http = require('http');
 var url = require("url");
 
-function start(route) {
+function start(route, handle) {
     function onRequest(request, response) { // 使用request和response对象处理HTTP请求的细节，并响应请求
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
 
-        route(pathname);
+        route(handle, pathname);
 
         response.writeHead(200, {"Content-Type": "text/plain"});
         response.write("Hello world"); // 响应请求，回复文本消息
