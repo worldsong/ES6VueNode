f1aa26acc38121b6dbb69dbb5d7aeb7bd3b04375
worldsong <772083612@qq.com>
2018-12-12 20:27:25 +0800

    不干活的HTTP服务器。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -1,7 +1,4 @@
 var http = require('http');
 
-http.createServer(function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello world");
-    response.end();
-}).listen(8888);
+var server = http.createServer();
+server.listen(8888);
