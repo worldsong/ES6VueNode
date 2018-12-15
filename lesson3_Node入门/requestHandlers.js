3f06b27a48dfef09527f4650f878e7102ad1b2c3
worldsong <772083612@qq.com>
2018-12-15 16:23:16 +0800

    重写/start的响应，显示一个文本区供用户输入内容，通过POST提交请求数据给服务器。

--- lesson3_Node入门/requestHandlers.js
+++ lesson3_Node入门/requestHandlers.js
@@ -1,22 +1,20 @@
-var exec = require('child_process').exec;
-
 function start(response) {
     console.log("Request handler 'start' was called.");
-    var content = "empty";
-    // 耗时任务，模拟阻塞
-    exec('tree C:/"Program Files"', {encoding: 'Song', timeout: 10000, maxBuffer: 20000 * 1024}, function (error, stdout, stderr) {
-        if(error){
-            console.log(error);
-            response.writeHead(500, {"Content-Type": "text/plain;charset=utf-8"});
-            response.write("错误500，服务器内部错误。"); // 响应请求，回复文本消息
-            response.end(); // 结束响应
-        } else {
-            content = stdout || stderr;
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write(content); // 响应请求，回复文本消息
-            response.end(); // 结束响应
-        }
-    })
+    var body = '<html>' +
+        '<head>' +
+        '<meta http-equiv="Content-Type" content="text/html; ' +
+        'charset=UTF-8" />' +
+        '</head>' +
+        '<body>' +
+        '<form action="/upload" method="post">' +
+        '<textarea name="text" rows="20" cols="60"></textarea>' +
+        '<input type="submit" value="Submit text" />' +
+        '</form>' +
+        '</body>' +
+        '</html>';
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(body); // 响应请求，回复文本消息
+    response.end(); // 结束响应
 }
 
 function upload(response) {
