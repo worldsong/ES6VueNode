c6160191fc33ed608b328109ca2d6c8c887615a0
worldsong <772083612@qq.com>
2018-12-15 15:56:22 +0800

    重构请求处理函数，使用response对象进行非阻塞异步响应。

--- lesson3_Node入门/requestHandlers.js
+++ lesson3_Node入门/requestHandlers.js
@@ -1,22 +1,30 @@
 var exec = require('child_process').exec;
 
-function start() {
+function start(response) {
     console.log("Request handler 'start' was called.");
     var content = "empty";
     // 耗时任务，模拟阻塞
-    exec('tree C:/"Program Files"', {encoding: 'Song', timeout: 10000, maxBuffer: 20000 * 1024}, function (error, stdout, stderr) {
+    exec('tree C:/', {encoding: 'Song', timeout: 10000, maxBuffer: 20000 * 1024}, function (error, stdout, stderr) {
         if(error){
-            console.log(error)
+            console.log(error);
+            response.writeHead(500, {"Content-Type": "text/plain"});
+            response.write("错误500，服务器内部错误。"); // 响应请求，回复文本消息
+            response.end(); // 结束响应
         } else {
             content = stdout || stderr;
+            response.writeHead(200, {"Content-Type": "text/plain"});
+            response.write(content); // 响应请求，回复文本消息
+            response.end(); // 结束响应
         }
     })
-    return content;
 }
 
-function upload() {
+function upload(response) {
     console.log("Request handler 'upload' was called.")
-    return "hello upload"
+    var content = "hello upload"
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write(content); // 响应请求，回复文本消息
+    response.end(); // 结束响应
 }
 
 exports.start = start;
