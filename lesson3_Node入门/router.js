c118fbaa234cfc27163fb2972c1f002a4b202815
worldsong <772083612@qq.com>
2018-12-15 15:55:17 +0800

    重构路由函数，传递response对象；并且响应“404”错误。

--- lesson3_Node入门/router.js
+++ lesson3_Node入门/router.js
@@ -1,10 +1,13 @@
-function route(handle, pathname) {
+function route(handle, pathname, response) {
     console.log("About to route a request for " + pathname);
     if(typeof handle[pathname] === 'function'){
-        return handle[pathname]();
+        handle[pathname](response);
     } else {
         console.log("No request handler found for " + pathname);
-        return "404 Not Found";
+        var content = "404 Not Found";
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write(content); // 响应请求，回复文本消息
+        response.end(); // 结束响应
     }
 }
 
