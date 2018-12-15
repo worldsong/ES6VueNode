3f988f14a3295b1a8e61fc9072c5d001c77d6aa3
worldsong <772083612@qq.com>
2018-12-15 15:51:28 +0800

    将response对象(从服务器的回调函数onRequest()获取)通过请求路由传递给请求处理程序，处理权下放；server.js=>router.js=>requestHandlers.js。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -6,11 +6,11 @@ function start(route, handle) {
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
 
-        var content = route(handle, pathname);
+        var content = route(handle, pathname, response);
 
-        response.writeHead(200, {"Content-Type": "text/plain"});
-        response.write(content); // 响应请求，回复文本消息
-        response.end(); // 结束响应
+        // response.writeHead(200, {"Content-Type": "text/plain"});
+        // response.write(content); // 响应请求，回复文本消息
+        // response.end(); // 结束响应
     }
 
     http.createServer(onRequest).listen(8888);
