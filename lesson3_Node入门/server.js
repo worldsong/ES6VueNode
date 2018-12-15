34d0517d3abc734119deddffe6d5d154494404a6
worldsong <772083612@qq.com>
2018-12-15 16:37:04 +0800

    设置接收数据的编码格式为UTF-8，注册“data”事件监听器，用于收集每次接收的新数据块，监听“end”事件发起路由，传递参数。

--- lesson3_Node入门/server.js
+++ lesson3_Node入门/server.js
@@ -3,10 +3,20 @@ var url = require("url");
 
 function start(route, handle) {
     function onRequest(request, response) { // 使用request和response对象处理HTTP请求的细节，并响应请求
+        var postData = "";
         var pathname = url.parse(request.url).pathname;
         console.log('Request for ' + pathname + ' received');
 
-        route(handle, pathname, response);
+        request.setEncoding("utf8");
+
+        request.addListener("data", function(postDataChunk){
+            postData += postDataChunk;
+            console.log("Received Post data chunk " + postDataChunk + ".");
+        });
+
+        request.addListener("end", function () {
+            route(handle, pathname, response, postData);
+        })
 
         // response.writeHead(200, {"Content-Type": "text/plain"});
         // response.write(content); // 响应请求，回复文本消息
