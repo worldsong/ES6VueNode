464a3a7a3455a750a8c264357ad1a511522b9df2
worldsong <772083612@qq.com>
2018-12-15 16:31:57 +0800

    代码优化：通过读取post_page.html文件的方式响应/start请求，不再做HTML页面字符串拼接。

--- lesson3_Node入门/requestHandlers.js
+++ lesson3_Node入门/requestHandlers.js
@@ -1,17 +1,8 @@
+var fs = require('fs');
+
 function start(response) {
     console.log("Request handler 'start' was called.");
-    var body = '<html>' +
-        '<head>' +
-        '<meta http-equiv="Content-Type" content="text/html; ' +
-        'charset=UTF-8" />' +
-        '</head>' +
-        '<body>' +
-        '<form action="/upload" method="post">' +
-        '<textarea name="text" rows="20" cols="60"></textarea>' +
-        '<input type="submit" value="Submit text" />' +
-        '</form>' +
-        '</body>' +
-        '</html>';
+    var body = fs.readFileSync('./post_page.html');
     response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
     response.write(body); // 响应请求，回复文本消息
     response.end(); // 结束响应
