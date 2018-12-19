b828ce72af284378aa112f0fc62bbc58f81de154
worldsong <772083612@qq.com>
2018-12-19 21:58:07 +0800

    防止有人使用浏览器(get)访问upload导致服务器崩溃。

--- lesson3_Node入门/uploadHandler.js
+++ lesson3_Node入门/uploadHandler.js
@@ -2,18 +2,26 @@ var fs = require('fs');
 var formidable = require('formidable');
 
 function upload(response, request) {
-    console.log("Request handler 'upload' was called.")
-    var form = new formidable.IncomingForm();
-    form.uploadDir = 'public/upload';
-    console.log('about to parse');
-    form.parse(request, function (err, fields, files) {
-        console.log("parsing done");
-        // 从缓存目录（public/upload）拷贝文件
-        fs.renameSync(files.MyFile.path, "tmp/test.png");
-        response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
-        response.write('received image:<br/>');
-        response.end("<img src ='/show'>");
-    })
+    if(request.method.toLowerCase() === 'get'){
+        console.log("No request handler found for upload in GET.");
+        response.writeHead(404, {"Content-Type": "text/plain"}); // 响应“404”错误
+        var content =  "404 Not found";
+        response.write(content); // 响应主体中发送文本消息
+        response.end(); // 结束响应
+    } else {
+        console.log("Request handler 'upload' was called.")
+        var form = new formidable.IncomingForm();
+        form.uploadDir = 'public/upload';
+        console.log('about to parse');
+        form.parse(request, function (err, fields, files) {
+            console.log("parsing done");
+            // 从缓存目录（public/upload）拷贝文件
+            fs.renameSync(files.MyFile.path, "tmp/test.png");
+            response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
+            response.write('received image:<br/>');
+            response.end("<img src ='/show'>");
+        })
+    }
 }
 
 exports.upload = upload;
