ff076807d555515372c3342ed50057b7fb49666d
worldsong <772083612@qq.com>
2018-12-22 16:54:49 +0800

    作业：使用Promise的方式做小说爬虫。

--- lesson4_Node爬虫/crawler.qu.la.js
+++ lesson4_Node爬虫/crawler.qu.la.js
@@ -3,9 +3,28 @@ var request = require('request');
 var cheerio = require('cheerio');
 var fs = require('fs');
 
-request('https://www.qu.la/book/3952/10529775.html', function (error, response, body) {
-    if(!error && response.statusCode === 200){
-        console.log(body);
-        fs.appendFileSync('qu.la.html', body)
+var URL = 'https://www.qu.la/book/3952/10529775.html';
+var Book_URL = 'https://www.qu.la/book/3952/';
+
+function myRequest(url, callback) {
+    var options = {
+        url: url,
+        encoding: null
+    }
+    request(options, callback)
+}
+
+myRequest(URL, function (error, response, body) {
+    if (!error && response.statusCode === 200) {
+        var html = body;
+        var $ = cheerio.load(html);
+        var content = $('div#content').text();
+        var urlNext = $('a#pager_next').attr('href');
+        var realURLNext = Book_URL + urlNext
+        console.log(realURLNext)
+        myRequest(realURLNext,function () {
+            myRequest(realURLNext)
+                ....
+        })
     }
 })
