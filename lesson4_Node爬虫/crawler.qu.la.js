dc60488be6a2d89b4d56d55a0102e0a052ccd9fc
worldsong <772083612@qq.com>
2018-12-22 16:41:46 +0800

    Node专用爬虫模块，request使用指南。

--- lesson4_Node爬虫/crawler.qu.la.js
+++ lesson4_Node爬虫/crawler.qu.la.js
@@ -1,20 +1,11 @@
 // www.qu.la 笔趣阁小说站点
-
-var https = require('https');
-var fs = require('fs');
+var request = require('request');
 var cheerio = require('cheerio');
+var fs = require('fs');
 
-https.get('https://www.qu.la/book/3952/10529775.html', function (response) {
-    var html = '';
-    response.setEncoding('utf8');
-    response.on('data', function (data) {
-        html += data;
-    });
-    response.on('end', function () {
-        // 页面内容获取完成，解析页面
-        console.log(html);
-        fs.appendFileSync('qu.la.html', html);
-    })
-}).on('error', function (e) {
-    console.log(e.message);
+request('https://www.qu.la/book/3952/10529775.html', function (error, response, body) {
+    if(!error && response.statusCode === 200){
+        console.log(body);
+        fs.appendFileSync('qu.la.html', body)
+    }
 })
