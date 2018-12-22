cf55c0ee396da2a9ca36d0e447cfcc4d4c067543
worldsong <772083612@qq.com>
2018-12-22 15:13:10 +0800

    Node爬虫：HTTP客户端编码及错误监听处理。

---
+++ lesson4_Node爬虫/http_get.js
@@ -0,0 +1,14 @@
+var http = require('http');
+var fs = require('fs');
+
+http.get('http://www.baidu.com/', function (response) {
+    // console.log(response);
+    response.setEncoding("utf8");
+    response.on('data', function (data) {
+        console.log(data);
+        // 将网站响应数据写入文件
+        fs.appendFile('baidu.html', data);
+    })
+}).on('error', function(e){
+    console.log(e.message)
+})
