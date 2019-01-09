8336436414f5e4f62d387e942bea61e77268ff4e
worldsong <772083612@qq.com>
2019-01-09 20:38:34 +0800

    在Vue-cli脚手架生成的项目中使用data属性。

--- lesson6_Vue基础/my-project/src/App.vue
+++ lesson6_Vue基础/my-project/src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>{{name}}</h1>
+    <p>{{message}}</p>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data(){
+      return {
+          name: 'Song',
+          message: 'Hello, Welcome to Vue.js'
+      }
   }
 }
 </script>
