1ed13f4316aed4d2f2a65dae0291c41a69f84d5a
worldsong <772083612@qq.com>
2019-01-16 21:41:49 +0800

    Vue2.x入门： 列表渲染，替换原数组触发视图更新。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -1,15 +1,63 @@
 var vm = new Vue({
     el: '#app',
-    data(){
-        return {
-            items: ['a', 'b', 'c']
+    data: {
+        books: [
+            {
+                title: 'Entertaining Kids Book',
+                price: 4.99,
+                id: 0,
+                genres: ['kids', 'fiction']
+            },
+            {
+                title: 'Teen Drama',
+                price: 5.99,
+                id: 1,
+                genres: ['teen', 'fiction']
+            },
+            {
+                title: 'Boring Facts',
+                price: 6.99,
+                id: 2,
+                genres: ['adult', 'non-fiction']
+            },
+            {
+                title: 'Overly Complex Story',
+                price: 7.99,
+                id: 3,
+                genres: ['adult', 'fiction']
+            },
+            {
+                title: 'Facts for Teens',
+                price: 3.99,
+                id: 4,
+                genres: ['teen', 'non-fiction']
+            }
+        ]
+    },
+    methods: {
+        teenFilter: function () {
+            this.books = this.books.filter(book => {
+                return (
+                    book.genres.findIndex( genre => {
+                        return genre === 'teen';
+                    }) >= 0
+                )
+            })
         }
     },
     template: `
-        <div>
-            <template v-for="item in items">
-                <h1>{{item}}</h1>
-            </template>
-        </div> 
+      <ul>
+        <li v-for="book in books" :key="book.id">
+          <p><strong>Id:</strong> {{ book.id }}<p>
+          <p><strong>Title:</strong> {{ book.title }}</p>
+          <p>
+            <strong>Genres:</strong>
+            <span v-for="(genre, index) in book.genres">
+                {{genre}} 
+                <span v-if="index < book.genres.length -1">, </span>
+            </span>
+          </p>
+        </li>
+      </ul>
     `
 })
