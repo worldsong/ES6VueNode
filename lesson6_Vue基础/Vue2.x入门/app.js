90afc4f0f65b2349a44740a6a465a44a1af3970a
worldsong <772083612@qq.com>
2019-01-16 20:32:57 +0800

    Vue2.x入门： 列表渲染，在数组上使用v-for指令的第二个参数:key，有利于优化渲染性能，尤其是重绘。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -1,13 +1,44 @@
 var app = new Vue({
     el: '#app',
     data: {
-        items: ['first', 'two', '3']
+        books: [
+            {
+                title: 'Entertaining Kids Book',
+                price: 4.99,
+                id: 0,
+                genres: ['kids', 'fiction']
+            },
+            {
+                title: 'Teen Drama',
+                price: 5.99,
+                id: 1,
+                genres: ['teen', 'fiction']
+            },
+            {
+                title: 'Boring Facts',
+                price: 6.99,
+                id: 2,
+                genres: ['adult', 'non-fiction']
+            },
+            {
+                title: 'Overly Complex Story',
+                price: 7.99,
+                id: 3,
+                genres: ['adult', 'fiction']
+            },
+            {
+                title: 'Facts for Teens',
+                price: 3.99,
+                id: 4,
+                genres: ['teen', 'non-fiction']
+            }
+        ]
     },
     template: `
             <div>
                  <ul>
-                     <li v-for="item in items">
-                        {{item}}
+                     <li v-for="book in books" :key="book.id">
+                        {{book}}
                      </li>
                 </ul>   
             </div>                    
