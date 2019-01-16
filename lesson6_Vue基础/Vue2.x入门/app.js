722f5745358b1fccf7509a6a033cd7e0eea30d15
worldsong <772083612@qq.com>
2019-01-16 20:26:27 +0800

    Vue2.x入门： 列表渲染，使用v-for指令渲染数组的每一个数据项。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -1,19 +1,15 @@
 var app = new Vue({
     el: '#app',
     data: {
-        yes: true,
-        no: false
+        items: ['first', 'two', '3']
     },
     template: `
             <div>
-                    <template v-if="no">
-                         <h1>Don't Show!</h1>
-                         <h1>It's a secret!</h1>
-                    </template>
-                    <template v-if="yes">
-                         <h1>Show!</h1>
-                         <h1>We like to Share!</h1>
-                    </template>
+                 <ul>
+                     <li v-for="item in items">
+                        {{item}}
+                     </li>
+                </ul>   
             </div>                    
             `
 })
