3e55cf105d577defed6b294e495c66a774bb14c3
worldsong <772083612@qq.com>
2019-01-23 20:19:13 +0800

    Vue2.x入门：利用ref属性实现数据的双向绑定。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -6,10 +6,12 @@ new Vue({
     },
     methods: {
         logName: function () {
-            console.log('you entered your name');
+            // console.log('you entered your name');
+            this.name = this.$refs.name.value;
         },
         logAge: function () {
-            console.log('you entered your age');
+            // console.log('you entered your age');
+            this.age = this.$refs.age.value;
         }
     }
 })
