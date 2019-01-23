b3595a6e0d426b8178d29b459d02a45fd5a3874a
worldsong <772083612@qq.com>
2019-01-23 20:22:34 +0800

    Vue2.x入门：使用v-model指令实现数据的双向绑定。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -5,13 +5,6 @@ new Vue({
         age: ''
     },
     methods: {
-        logName: function () {
-            // console.log('you entered your name');
-            this.name = this.$refs.name.value;
-        },
-        logAge: function () {
-            // console.log('you entered your age');
-            this.age = this.$refs.age.value;
-        }
+
     }
 })
