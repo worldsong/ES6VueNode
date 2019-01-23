fdae2c7be94f81c428cfacea58e96de2c1a46865
worldsong <772083612@qq.com>
2019-01-23 21:24:55 +0800

    Vue2.x入门：组件的引入。

--- lesson6_Vue基础/Vue2.x入门/app.js
+++ lesson6_Vue基础/Vue2.x入门/app.js
@@ -1,30 +1,10 @@
+Vue.component('greeting', {
+    template: '<p>组件应用</p>'
+})
 var one = new Vue({
     el:'#vue-app-one',
-    data: {
-        title: 'Vue app One'
-    },
-    computed: {
-        greet: function () {
-            return 'Hello, From app one :)'
-        }
-    }
 });
 
 var two = new Vue({
     el:'#vue-app-two',
-    data: {
-        title: 'Vue app Two'
-    },
-    computed: {
-        greet: function () {
-            return 'Hello, From app Two :)'
-        }
-    },
-    methods: {
-        changeTitle: function () {
-            one.title = 'Title Changed.'
-        }
-    }
 })
-
-two.title = 'Changed from outside'
