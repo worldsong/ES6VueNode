b310413960c00927e07c1171ee6a0e36afd01027
worldsong <772083612@qq.com>
2019-02-20 20:31:40 +0800

    路由精讲：二级路由。

---
+++ lesson6_Vue基础/pizza-app/src/components/about/OrderingGuide.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>点餐文档</h1>
+</template>
--- lesson6_Vue基础/pizza-app/src/main.js
+++ lesson6_Vue基础/pizza-app/src/main.js
@@ -11,6 +11,12 @@ import About from './components/about/About.vue'
 import Login from './components/Login.vue'
 import Register from './components/Register.vue'
 
+// 二级路由
+import Contact from './components/about/Contact.vue'
+import Delivery from './components/about/Delivery.vue'
+import History from './components/about/History.vue'
+import OrderingGuide from './components/about/OrderingGuide.vue'
+
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
@@ -19,7 +25,14 @@ const routes = [
   {path: '/',name:"homeLink", component: Home},
   {path: '/menu',name:"menuLink", component: Menu},
   {path: '/admin',name:"adminLink", component: Admin},
-  {path: '/about',name:"aboutLink", component: About},
+  {path: '/about',name:"aboutLink", component: About, children: [
+    // {path: '/about/contact', name: "contactLink", component: Contact},
+    // {path: 'contact', name: "contactLink", component: Contact},
+    {path: '/contact', name: "contactLink", component: Contact},
+    {path: 'history', name: "historyLink", component: History},
+    {path: 'delivery', name: "deliveryLink", component: Delivery},
+    {path: 'orderingGuide', name: "orderingGuideLink", component: OrderingGuide}
+  ]},
   {path: '/login',name:"loginLink", component: Login},
   {path: '/register',name:"registerLink", component: Register},
   {path: '*', redirect: '/'},
