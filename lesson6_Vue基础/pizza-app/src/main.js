32c640b27d5b9336c82d6a9df8676130b1fd2a1e
worldsong <772083612@qq.com>
2019-02-20 21:36:49 +0800

    路由精讲：抽离路由，优化代码结构。

--- lesson6_Vue基础/pizza-app/src/main.js
+++ lesson6_Vue基础/pizza-app/src/main.js
@@ -3,60 +3,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import App from './App'
-
-import Home from './components/Home.vue'
-import Menu from './components/Menu.vue'
-import Admin from './components/Admin.vue'
-import About from './components/about/About.vue'
-import Login from './components/Login.vue'
-import Register from './components/Register.vue'
-
-// 二级路由
-import Contact from './components/about/Contact.vue'
-import Delivery from './components/about/Delivery.vue'
-import History from './components/about/History.vue'
-import OrderingGuide from './components/about/OrderingGuide.vue'
-
-// 三级路由
-import Phone from './components/about/contact/Phone.vue'
-import PersonName from './components/about/contact/PersonName.vue'
+import {routes} from './routes'
 
 Vue.config.productionTip = false
 
 Vue.use(VueRouter);
-
-const routes = [
-  {path: '/',name:"homeLink", component: Home},
-  {path: '/menu',name:"menuLink", component: Menu},
-  {path: '/admin',name:"adminLink", component: Admin,
-    // beforeEnter: (to, from, next) => {
-    //   // 路由的独享守卫
-    //   // alert("非登录状态，不能访问此页面");
-    //   // next(false);
-    //
-    //   if(to.path== '/login' || to.path == '/register'){
-    //     next();
-    //   }else {
-    //     alert("还没有登录，请先登录！");
-    //     next('/login');
-    //   }
-    // }
-  },
-  {path: '/about',name:"aboutLink", redirect:'/contact' , component: About, children: [
-    // {path: '/about/contact', name: "contactLink", component: Contact},
-    // {path: 'contact', name: "contactLink", component: Contact},
-    {path: '/contact', name: "contactLink", redirect:'/personName' , component: Contact, children: [
-      {path: 'phone', name: "phoneNumber", component: Phone},
-      {path: '/personName', name: "personName", component: PersonName},
-    ]},
-    {path: 'history', name: "historyLink", component: History},
-    {path: 'delivery', name: "deliveryLink", component: Delivery},
-    {path: 'orderingGuide', name: "orderingGuideLink", component: OrderingGuide}
-  ]},
-  {path: '/login',name:"loginLink", component: Login},
-  {path: '/register',name:"registerLink", component: Register},
-  {path: '*', redirect: '/'},
-]
 
 const router = new VueRouter({
   routes,
@@ -80,9 +31,9 @@ router.beforeEach((to, from, next) => {
 */
 
 // 后置钩子
-router.afterEach((to, from) => {
-  alert("after each")
-})
+// router.afterEach((to, from) => {
+//   alert("after each")
+// })
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
